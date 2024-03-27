5552ced2f747b6f32e16b4ec07964da2cb32c01c
Joshua Matthews <joshua.matthews@patracorp.com>
2024-03-27 12:07:58 -0500

    fixed small issues. fleshed out more functions

--- c_str.c
+++ c_str.c
@@ -1,4 +1,7 @@
 #include "c_str.h"
+#include <string.h>
+
+#define ARRAY_INC_CONSTANT 1.2
 
 struct internal_c_str {
     char* data;
@@ -6,48 +9,49 @@ struct internal_c_str {
     size_t cap;
 };
 
-
 c_str_error append(struct c_str* data, const char* str, size_t len) {
-    size_t d_len = data->_internal.len;
+    size_t d_len = data->_internal->len;
     size_t new_len = d_len + len;
-    if (new_len > data->_internal.cap) {
-        size_t new_cap = ((double)data->_internal.cap * 1.2);
-        data->_internal.data = realloc(data->_internal.data, sizeof(char*) * new_cap);
-        data->_internal.cap = new_cap;
-        if (data->_internal.data == NULL) {
+    if (new_len > data->_internal->cap) {
+        size_t new_cap = ((double)data->_internal->cap * ARRAY_INC_CONSTANT);
+        data->_internal->data = realloc(
+                data->_internal->data, sizeof(char) * new_cap);
+        data->_internal->cap = new_cap;
+        if (data->_internal->data == NULL) {
             return C_STR_MALLOC_ERROR;
         }
     }
-    strncpy(&data->_internal.data[d_len - 1], str, len);
-    data->_internal.data[new_len] = '\0';
+    strncpy(&data->_internal->data[d_len - 1], str, len);
+    data->_internal->data[new_len] = '\0';
     return C_STR_NO_ERROR;
 }
 
 int length(const struct c_str* data) {
     // subtract null-byte
-    return data->_internal.len - 1;
-}
-
-c_str_error get_const_str(const struct c_str* data, const char ** out) {
-    *out = data->_internal.data;
-    return C_STR_NO_ERROR;
+    return data->_internal->len - 1;
 }
 
 c_str_error get_str(const struct c_str* data, char** out) {
-    *out = data->_internal.data;
+    *out = data->_internal->data;
+    return C_STR_NO_ERROR;
+}
+
+c_str_error get_const_str(const struct c_str* data, const char ** out) {
+    *out = data->_internal->data;
     return C_STR_NO_ERROR;
 }
 
 c_str_error set(struct c_str* data, const char *str, size_t len) {
+    // TODO set the string and account for resizing if needed.
     return C_STR_NO_ERROR;
 }
 
 c_str_error at(const struct c_str* data, size_t idx, char* out) {
-    const int len = data->_internal.len;
+    const int len = data->_internal->len;
     if (idx > len || idx < 0) {
         return C_STR_OUT_OF_BOUNDS;
     }
-    *char = data->_internal.data[idx];
+    *out = data->_internal->data[idx];
     return C_STR_NO_ERROR;
 }
 
@@ -57,11 +61,34 @@ c_str_error new_c_str(struct c_str* data, size_t cap) {
     }
     // add for null-terminator
     ++cap;
-    char *tmp = (char*)malloc(sizeof(char*)*cap);
+    char *tmp = (char*)malloc(sizeof(char)*cap);
+    if (tmp == NULL) {
+        return C_STR_MALLOC_ERROR;
+    }
     tmp[0] = '\0';
-    data->_internal.data = tmp;
-    data->_internal.len = 1;
-    data->_internal.cap = cap;
+    data->_internal = (struct internal_c_str*)malloc(sizeof(struct internal_c_str));
+    if (data->_internal == NULL) {
+        return C_STR_MALLOC_ERROR;
+    }
+    data->_internal->data = tmp;
+    data->_internal->len = 1;
+    data->_internal->cap = cap;
     return C_STR_NO_ERROR;
+}
+
+c_str_error new_c_str_with_string(struct c_str* data, const char* str, size_t len) {
+    size_t cap = ((double)len * ARRAY_INC_CONSTANT);
+    c_str_error err = new_c_str(data, cap);
+    if (err!=C_STR_NO_ERROR) {
+        return err;
+    }
+    data->set(data, str, len);
+    return C_STR_NO_ERROR;
+}
+
+void free_c_str(struct c_str* data) {
+    free(data->_internal->data);
+    free(data->_internal);
+    data->_internal = NULL;
 }
 
--- c_str.h
+++ c_str.h
@@ -1,24 +1,98 @@
 #ifndef CUSTOM_C_STR
 #define CUSTOM_C_STR
 
+#include <stdlib.h>
+
+/**
+ * Enums for error c_str error codes.
+ */
 typedef enum c_str_error {
+    /* success */
     C_STR_NO_ERROR,
+    /* malloc error */
     C_STR_MALLOC_ERROR,
+    /* out of bounds */
     C_STR_OUT_OF_BOUNDS
 } c_str_error;
 
+/**
+ * Internal c_str structure.
+ */
 struct internal_c_str;
 
+/**
+ * Structure to handle dealing with strings in a more convenient way.
+ */
 typedef struct c_str {
-    internal_c_str _internal;
+    /* internal */
+    struct internal_c_str *_internal;
+    /**
+     * Append the given string to this string.
+     * @param[in] struct c_str* The c_str object.
+     * @param[in] const char* The string to append.
+     * @param[in] size_t The size of the string to copy.
+     * @returns c_str_error.
+     */
     c_str_error (*append)(struct c_str*, const char *, size_t);
+    /**
+     * Get the length of the string. (does not include null-terminator
+     * @param[in] const struct c_str* The c_str structure.
+     * @returns The length.
+     */
     int (*length)(const struct c_str*);
+    /**
+     * Get the underlying string.
+     * @param[in] const struct c_str* The c_str structure.
+     * @param[out] char** The pointer to return out the string to.
+     * @returns c_str_error.
+     */
     c_str_error (*get_str)(const struct c_str*, char **);
-    c_str_error (*get_str)(const struct c_str*, const char **);
+    /**
+     * Get the underlying string as a const char*.
+     * @param[in] const struct c_str* The c_str structure.
+     * @param[out] const char** The pointer to return out the string to.
+     * @returns c_str_error.
+     */
+    c_str_error (*get_const_str)(const struct c_str*, const char **);
+    /**
+     * Set the underlying string with the given string.
+     * @param[in] struct c_str* The c_str structure.
+     * @param[in] const char* The string data.
+     * @param[in] size_t The size of the given string.
+     * @returns c_str_error.
+     */
     c_str_error (*set)(struct c_str*, const char *, size_t);
+    /**
+     * Get the character at the given index.
+     * @param[in] struct c_str* The c_str structure.
+     * @param[in] size_t The index value.
+     * @param[out] char* The out param.
+     * @returns c_str_error.
+     */
     c_str_error (*at)(struct c_str*, size_t, char*);
 } c_str;
 
+/**
+ * Initializes a new c_str structure with a certain size.
+ * @param[in] struct c_str* The c_str structure to initialize.
+ * @param[in] size_t The size to initialize to.
+ * @returns c_str_error.
+ */
 c_str_error new_c_str(struct c_str*, size_t);
+
+/**
+ * Initializes a new c_str structure with a given string.
+ * @param[in] struct c_str* The c_str structure to initialize.
+ * @param[in] const char* The string to copy.
+ * @param[in] size_t The size of the given string.
+ * @returns c_str_error.
+ */
+c_str_error new_c_str_with_string(struct c_str*, const char*, size_t);
+
+/**
+ * Frees the internals of the c_str.
+ * @param[in] struct c_str* The c_str to free.
+ */
+void free_c_str(struct c_str*);
 
 #endif
