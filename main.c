62ff81f5128ff34422ebd01765236c6ee064e1d3
Joshua Matthews <jmatth93@gmail.com>
2024-03-31 18:09:51 -0500

    fixed stuff, added more tests

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 #include <assert.h>
 
 #define TEST_STR "test string\0"
-#define TEST_STR_LEN 12
 
 static char* test_str() {
-  char* tmp = malloc(sizeof(char) * TEST_STR_LEN);
-  strcpy(tmp, TEST_STR);
+  size_t len = strlen(TEST_STR);
+  char* tmp = malloc(sizeof(char) * len);
+  strncpy(tmp, TEST_STR, len);
   return tmp;
 }
 
@@ -57,6 +57,28 @@ static void test_new_c_str_with_string_normal() {
   free_c_str(&value);
 }
 
+static void test_new_c_str_with_string_less_than_or_equal_to_zero() {
+  c_str value;
+  char* test = test_str();
+  size_t test_len = 0;
+  c_str_error err = new_c_str_with_string(&value, test, test_len);
+  assert(err == C_STR_NO_ERROR);
+  assert(value.length(&value) == 1);
+  char* result;
+  value.get_str(&value, &result);
+  assert(strcmp("t", result) == 0);
+  free(test);
+  free_c_str(&value);
+}
+
+static void test_free_c_str() {
+  c_str value;
+  c_str_error err = new_c_str(&value, 1);
+  assert(err == C_STR_NO_ERROR);
+  free_c_str(&value);
+  assert(value._internal == NULL);
+}
+
 int main(int argc, char *argv[]) {
 
   test_new_c_str_normal();
@@ -64,6 +86,9 @@ int main(int argc, char *argv[]) {
   test_new_c_str_greater_than_one();
 
   test_new_c_str_with_string_normal();
+  test_new_c_str_with_string_less_than_or_equal_to_zero();
+
+  test_free_c_str();
 
   return 0;
 }
