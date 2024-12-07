c9108568a10fdc816512dde9041199ee46300a0e
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:23:40 -0600

    finished fleshing out, but haven't tested

--- c_str.c
+++ c_str.c
@@ -8,8 +8,18 @@ struct internal_c_str {
 
 
 c_str_error append(struct c_str* data, const char* str, size_t len) {
-    // TODO append string and appropriately set null-terminator
-    // adjust cap if needed
+    size_t d_len = data->_internal.len;
+    size_t new_len = d_len + len;
+    if (new_len > data->_internal.cap) {
+        size_t new_cap = ((double)data->_internal.cap * 1.2);
+        data->_internal.data = realloc(data->_internal.data, sizeof(char*) * new_cap);
+        data->_internal.cap = new_cap;
+        if (data->_internal.data == NULL) {
+            return C_STR_MALLOC_ERROR;
+        }
+    }
+    strncpy(&data->_internal.data[d_len - 1], str, len);
+    data->_internal.data[new_len] = '\0';
     return C_STR_NO_ERROR;
 }
 
@@ -41,8 +51,17 @@ c_str_error at(const struct c_str* data, size_t idx, char* out) {
     return C_STR_NO_ERROR;
 }
 
-c_str_error new_c_str_error(struct c_str* data, size_t cap) {
-    // TODO malloc internal data structure with cap
+c_str_error new_c_str(struct c_str* data, size_t cap) {
+    if (cap < 0) {
+        cap = 0;
+    }
+    // add for null-terminator
+    ++cap;
+    char *tmp = (char*)malloc(sizeof(char*)*cap);
+    tmp[0] = '\0';
+    data->_internal.data = tmp;
+    data->_internal.len = 1;
+    data->_internal.cap = cap;
     return C_STR_NO_ERROR;
 }
 
